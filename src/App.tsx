c19d3b4dd7c76bbb4959a2430ffe1907a0f8d950
Kalafathakan <98932470+Kalafathakan@users.noreply.github.com>
2022-09-06 17:31:50 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { useState } from 'react';
+
+import Home from './pages/Home';
+import Footer from './pages/Footer';
+import Services from './pages/Services';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='services' element={<Services />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+      </Routes>
+      <Footer />
+      <div id="bg"></div>
     </div>
   );
 }
